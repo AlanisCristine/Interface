be66a643709534f2cf3bab0b3245a9f13a00eaea
Alanis Cristine <163910391+AlanisCristine@users.noreply.github.com>
2024-10-10 16:07:19 -0300

    Exercício 2 -  Resposta

---
+++ eXEMPLO3/Cheque.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo3
+{
+    public class Cheque : IFormaPagamento
+    {
+        public void CalculaValorPagamento(double valorTotal)
+        {
+            Console.WriteLine($"Como pagamento será realizado através de Cheque, vamos oferecer 15% de desconto, Valor Final será: {valorTotal - (valorTotal * 0.15)}");
+        }
+    }
+}
