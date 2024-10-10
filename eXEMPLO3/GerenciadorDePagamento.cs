49c13b3ac2e65c843f1efc89c50244b9e3a2e6af
Alanis Cristine <163910391+AlanisCristine@users.noreply.github.com>
2024-10-10 16:25:25 -0300

    Exercício 3 - Resposta

---
+++ eXEMPLO3/GerenciadorDePagamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo3
+{
+    public class GerenciadorDePagamento
+    {
+        public void ProcessarPagamento(IFormaPagamento formaPagamento, double valorTotal)
+        {
+            formaPagamento.CalculaValorPagamento(200.0);
+        }
+    }
+}
